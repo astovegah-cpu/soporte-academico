ecbb3bc71242ca1fbf5f6400c094dd36c0b8afdb
Hillary <astovegah@gmail.com>
2026-09-22 11:15:13 -0500

    Req. 2: valida codigo del estudiante

--- main.py
+++ main.py
@@ -1,25 +1,31 @@
+def validar_codigo(codigo):
+    if codigo == "":
+        return False
+
+    if len(codigo) < 8:
+        return False
+
+    return True
+
+
 def registrar_solicitud():
     print("\n--- REGISTRO DE SOLICITUD ---")
 
     codigo = input("Ingrese código del estudiante: ")
+
+    while not validar_codigo(codigo):
+        print("Código inválido. Debe tener al menos 8 caracteres.")
+        codigo = input("Ingrese código del estudiante: ")
+
     nombre = input("Ingrese nombre del estudiante: ")
     tipo_consulta = input("Ingrese tipo de consulta: ")
     descripcion = input("Ingrese una descripción breve: ")
 
-    solicitud = {
-        "codigo": codigo,
-        "nombre": nombre,
-        "tipo_consulta": tipo_consulta,
-        "descripcion": descripcion
-    }
-
-    return solicitud
+    print("\n--- SOLICITUD REGISTRADA ---")
+    print("Código:", codigo)
+    print("Nombre:", nombre)
+    print("Tipo de consulta:", tipo_consulta)
+    print("Descripción:", descripcion)
 
 
-solicitud = registrar_solicitud()
-
-print("\n--- SOLICITUD REGISTRADA ---")
-print("Código:", solicitud["codigo"])
-print("Nombre:", solicitud["nombre"])
-print("Tipo de consulta:", solicitud["tipo_consulta"])
-print("Descripción:", solicitud["descripcion"])
+registrar_solicitud()
